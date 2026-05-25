780871638bb9ad27876671ebf52f751a0b513164
ezranyaosi522-byte <ezranyaosi522@gmail.com>
2026-05-25 15:57:14 +0300

    Add files via upload

---
+++ for_loop_100_to_50.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // Student details
+    printf("Name: Ezra Nyaosi\n");
+    printf("Registration Number: BDSA-03-0029/2026\n");
+    printf("Course: Bachelor of Data Science and Analytics\n\n");
+
+    // For loop from 100 down to 50
+    for (int i = 100; i >= 50; i--) {
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
